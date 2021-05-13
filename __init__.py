9a06f371e7b6034b07fd0c62641220926bf22afd
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2021-05-13 21:51:56 +0000

    create

--- __init__.py
+++ __init__.py
@@ -1,2 +1,9 @@
 import numpy as np
-import pandas as pd
+from scipy.integrate import odeint,solve_ivp
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from matplotlib import rc
+rc("animation",html="jshtml")
+from matplotlib.patches import Rectangle
+import warnings
+warnings.filterwarnings("ignore")
