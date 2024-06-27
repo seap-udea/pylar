ed72178e9259035a1a4667bd3a56f3a9b7109662
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2024-06-27 00:55:00 -0500

    [VER] Version 1.0.3 released

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
-    version='1.0.2',
+    version='1.0.3',
 
     # ######################################################################
     # FILES
